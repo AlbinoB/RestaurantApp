ec966f7360398d9bf2de0d37b78eed0243a8f490
AlbinoB <albinobraganza21@gmail.com>
2020-03-05 19:54:46 +0530

    Restaurant Menu bugs fixed

--- app/src/main/java/com/albino/restaurantapp/activity/RestaurantMenuActivity.kt
+++ app/src/main/java/com/albino/restaurantapp/activity/RestaurantMenuActivity.kt
@@ -1,31 +1,176 @@
 package com.albino.restaurantapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.albino.restaurantapp.R
-import com.albino.restaurantapp.fragment.RestaurantMenuFragment
+import com.albino.restaurantapp.adapter.RestaurantMenuAdapter
+import com.albino.restaurantapp.model.RestaurantMenu
+import com.albino.restaurantapp.utils.ConnectionManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
 
 class RestaurantMenuActivity : AppCompatActivity() {
+
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var layoutManager: RecyclerView.LayoutManager
+    lateinit var menuAdapter: RestaurantMenuAdapter
+    lateinit var restaurantId:String
+
+    var restaurantMenuList = arrayListOf<RestaurantMenu>()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant_menu)
-        openDashboard()
+
+
+        //openDashboard()
+
+
+        restaurantId = intent.getStringExtra("restaurantId")
+
+
+
+        layoutManager = LinearLayoutManager(this)//set the layout manager
+
+        recyclerView = findViewById(R.id.recyclerViewRestaurantMenu)
+
+
+
+        if (ConnectionManager().checkConnectivity(this)) {
+
+            try {
+
+                val queue = Volley.newRequestQueue(this)
+
+                val restaurantMenuActivity = RestaurantMenuActivity()
+
+
+                //val restaurantId:String=""
+
+                val url = "http://13.235.250.119/v2/restaurants/fetch_result/" + restaurantId
+
+                val jsonObjectRequest = object : JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    null,
+                    Response.Listener {
+                        println("Response12menu is " + it)
+
+                        val responseJsonObjectData = it.getJSONObject("data")
+
+                        val success = responseJsonObjectData.getBoolean("success")
+
+                        if (success) {
+
+                            val data = responseJsonObjectData.getJSONArray("data")
+
+                            for (i in 0 until data.length()) {
+                                val bookJsonObject = data.getJSONObject(i)
+                                val menuObject = RestaurantMenu(
+                                    bookJsonObject.getString("id"),
+                                    bookJsonObject.getString("name"),
+                                    bookJsonObject.getString("cost_for_one")
+
+                                )
+                                restaurantMenuList.add(menuObject)
+
+                                //progressBar.visibility = View.GONE
+
+                                menuAdapter = RestaurantMenuAdapter(
+                                    this,
+                                    restaurantMenuList
+                                )//set the adapter with the data
+
+                                recyclerView.adapter =
+                                    menuAdapter//bind the  recyclerView to the adapter
+
+                                recyclerView.layoutManager =
+                                    layoutManager //bind the  recyclerView to the layoutManager
+
+
+                                //spacing between list items
+                                /*recyclerDashboard.addItemDecoration(
+                                    DividerItemDecoration(
+                                        recyclerDashboard.context,(layoutManager as LinearLayoutManager).orientation
+                                    )
+                                )*/
+                            }
+
+
+                        }
+                    },
+                    Response.ErrorListener {
+                        println("Error12menu is " + it)
+
+                        Toast.makeText(
+                            this,
+                            "Some Error occurred!!!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }) {
+                    override fun getHeaders(): MutableMap<String, String> {
+                        val headers = HashMap<String, String>()
+
+                        headers["Content-type"] = "application/json"
+                        headers["token"] = "acdc385cfd7264"
+
+                        return headers
+                    }
+                }
+
+                queue.add(jsonObjectRequest)
+
+            } catch (e: JSONException) {
+                Toast.makeText(
+                    this,
+                    "Some Unexpected error occured!!!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        } else {
+            val alterDialog = androidx.appcompat.app.AlertDialog.Builder(this)
+
+            alterDialog.setTitle("No Internet")
+            alterDialog.setMessage("Internet Connection can't be establish!")
+            alterDialog.setPositiveButton("Open Settings") { text, listener ->
+                val settingsIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)//open wifi settings
+                startActivity(settingsIntent)
+                finish()
+            }
+
+
+        }
+
+        fun openDashboard() {
+            val transaction = supportFragmentManager.beginTransaction()
+
+
+
+
+            transaction.replace(
+                R.id.frameLayout,
+                RestaurantMenuFragment()
+            )//replace the old layout with the new frag  layout
+
+            transaction.commit()//apply changes
+
+            supportActionBar?.title = "Menu"//change the title when each new fragment is opened
+
+
+        }
     }
 
-    fun openDashboard(){
-        val transaction=supportFragmentManager.beginTransaction()
-
-        transaction.replace(
-            R.id.frameLayout,
-            RestaurantMenuFragment()
-        )//replace the old layout with the new frag  layout
-
-        transaction .commit()//apply changes
-
-        supportActionBar?.title="Menu"//change the title when each new fragment is opened
-
-
-    }
 
 }
