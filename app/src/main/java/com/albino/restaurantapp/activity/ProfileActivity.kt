60f3b23f5296638ebe404d347c3ca54f9caefbb6
albinoB <albinobraganza21@gmail.com>
2020-03-08 13:01:12 +0530

    FAQ

---
+++ app/src/main/java/com/albino/restaurantapp/activity/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.albino.restaurantapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.albino.restaurantapp.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
